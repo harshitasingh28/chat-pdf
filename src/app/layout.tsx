9d7828d8b451868feb2a538bd0ec689e8eead855
Harshita Singh <harshitaaa2807@gmail.com>
2026-06-12 08:27:07 +0530

    feat: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "@/components/Providers";
 import {Toaster} from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,10 +33,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
       </Providers>
       <Toaster/>
     </ClerkProvider>
+    
   );
 }
